88e319a7dff5c7e99d923453826491494ef49799
dmitriy3305 <dmitrijkrymskij6@gmail.com>
2023-11-07 01:30:13 +0400

    fix: styles

--- src/components/characterData.tsx
+++ src/components/characterData.tsx
@@ -22,35 +22,43 @@ const CharacterData: React.FC<CharacterDataProps> = ({ results }) => {
       {results.map((result, index) => (
         <div key={result.name} className="results-items">
           <h1 className="results-item">{result.name}</h1>
-          <button
-            className="show-details"
-            onClick={() => handleShowDetails(index)}
-          >
-            {selectedDetailIndex === index ? 'hide' : 'show details'}
-          </button>
-          {isLoading ? (
-            <div className="loader"></div>
-          ) : selectedDetailIndex === index ? (
-            <>
-              {characterData.find(
-                (character) => character.name === result.name
-              ) ? (
-                <div className="characters-detail">
-                  {result.height && <p>Height: {result.height}</p>}
-                  {result.hair_color && <p>Hair color: {result.hair_color}</p>}
-                  {result.skin_color && <p>Skin color: {result.skin_color}</p>}
-                  {result.eye_color && <p>Eye color: {result.eye_color}</p>}
-                  {result.birth_year && <p>Birth year: {result.birth_year}</p>}
-                  {result.gender && <p>Gender: {result.gender}</p>}
-                </div>
-              ) : (
-                <Details
-                  setCharacterData={setCharacterData}
-                  name={result.name}
-                />
-              )}
-            </>
-          ) : null}
+          <div className="details-container">
+            <button
+              className="show-details"
+              onClick={() => handleShowDetails(index)}
+            >
+              {selectedDetailIndex === index ? 'hide' : 'show details'}
+            </button>
+            {isLoading ? (
+              <div className="loader"></div>
+            ) : selectedDetailIndex === index ? (
+              <>
+                {characterData.find(
+                  (character) => character.name === result.name
+                ) ? (
+                  <div className="characters-detail">
+                    {result.height && <p>Height: {result.height}</p>}
+                    {result.hair_color && (
+                      <p>Hair color: {result.hair_color}</p>
+                    )}
+                    {result.skin_color && (
+                      <p>Skin color: {result.skin_color}</p>
+                    )}
+                    {result.eye_color && <p>Eye color: {result.eye_color}</p>}
+                    {result.birth_year && (
+                      <p>Birth year: {result.birth_year}</p>
+                    )}
+                    {result.gender && <p>Gender: {result.gender}</p>}
+                  </div>
+                ) : (
+                  <Details
+                    setCharacterData={setCharacterData}
+                    name={result.name}
+                  />
+                )}
+              </>
+            ) : null}
+          </div>
         </div>
       ))}
     </div>
