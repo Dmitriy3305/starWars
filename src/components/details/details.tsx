fee1cd0e1b748b86bb655639669613ecc7815a6b
dmitriy3305 <dmitrijkrymskij6@gmail.com>
2023-11-19 18:07:27 +0400

    feat: add detailsReduser, flag

--- src/components/details/details.tsx
+++ src/components/details/details.tsx
@@ -1,13 +1,23 @@
 import { useEffect } from 'react';
 import { SearchResult, DetailsProps } from '../../types';
+import { useDispatch } from 'react-redux';
+import {
+  setSearchResults,
+  startLoading,
+  endLoading,
+} from '../../reducers/detailsReduser';
 
 const Details = ({ name, setCharacterData }: DetailsProps) => {
+  const dispatch = useDispatch();
   const encodedName = name.replace(/ /g, '%20');
   useEffect(() => {
+    dispatch(startLoading());
     const url = `https://swapi.dev/api/people/?search=${encodedName}`;
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
+        dispatch(setSearchResults(data));
+        dispatch(endLoading());
         setCharacterData((prevState: SearchResult[]) => {
           if (prevState.length === 0) {
             return data.results;
@@ -23,6 +33,9 @@ const Details = ({ name, setCharacterData }: DetailsProps) => {
       })
       .catch((error) => {
         console.log('Error:', error);
+      })
+      .finally(() => {
+        dispatch(endLoading());
       });
   }, [name, setCharacterData]);
 
--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import searchReducer from './searchReducer';
 import dataReducer from './dataReducer';
+import detailsReduser from './detailsReduser';
 
 const rootReducer = combineReducers({
   search: searchReducer,
   data: dataReducer,
+  details: detailsReduser,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
