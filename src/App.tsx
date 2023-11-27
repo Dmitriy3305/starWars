edac002018c21a4c11fc68ae07e7fbeab07c867b
dmitrij <dmitrijkrymskij6@gmail.com>
2023-11-27 21:37:40 +0400

    refactror: app, navbar

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,6 @@ import './App.css';
 import Navbar from './components/navbar/navbar';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <BrowserRouter>
       <Navbar />
