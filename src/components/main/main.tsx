8705bdd1b1c2aaf15b552cda67acdfee06bdbf7b
dmitrij <dmitrijkrymskij6@gmail.com>
2023-12-03 21:23:43 +0400

    fix: main component

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../reducers/rootReducer';
 import ImageFromBase64 from './upload';
+import styles from './main.module.css';
 
 const Main: FC = () => {
   const submissions = useSelector(
@@ -9,22 +10,28 @@ const Main: FC = () => {
   );
 
   return (
-    <div>
-      <h1>Unconrolled</h1>
-      {submissions.map((submission, index) => (
-        <div key={index}>
-          <p>Name: {submission.name as string}</p>
-          <p>Age: {submission.age as string}</p>
-          <p>Email: {submission.name as string}</p>
-          <p>Gender: {submission.gender as string}</p>
-          <p>Accept T&C: {submission.tc as string}</p>
-          <p>
-            Upload:
-            <ImageFromBase64 base64={submission.upload as string} />
-          </p>
-          <p>Country: {submission.country as string}</p>
-        </div>
-      ))}
+    <div className={styles.wrapper}>
+      <div className={styles.uncontrolled}>
+        <h2>Unconrolled</h2>
+        {submissions.map((submission, index) => (
+          <div key={index} className={styles.list}>
+            <p>Name: {submission.name as string}</p>
+            <p>Age: {submission.age as string}</p>
+            <p>Email: {submission.name as string}</p>
+            <p>Password: {submission.password as string}</p>
+            <p>Gender: {submission.gender as string}</p>
+            <p>Accept T&C: {submission.tc as string}</p>
+            <div className={styles.upload}>
+              <p>Upload:</p>
+              <ImageFromBase64 base64={submission.upload as string} />
+            </div>
+            <p>Country: {submission.country as string}</p>
+          </div>
+        ))}
+      </div>
+      <div className={styles.controlled}>
+        <h2>Conrolled</h2>
+      </div>
     </div>
   );
 };
